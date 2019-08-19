1156b2fc3f886e7b2f10ba85a4a6b98f531d180c
张实唯 <ylxdzsw@gmail.com>
2019-08-19 18:43:39 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,13 +1,9 @@
 const Meta = imports.gi.Meta
-const Mainloop = imports.mainloop
 
 let handle
 
 function maximize(act) {
     const win = act.meta_window
-
-    if (win.window_type !== Meta.WindowType.NORMAL)
-        return
     
     if (global.workspace_manager.get_active_workspace().list_windows().length == 1)
         return
@@ -15,25 +11,22 @@ function maximize(act) {
     if (global.display.get_primary_monitor() != win.get_monitor())
         return
     
-    let lastworkspace = global.workspace_manager.n_workspaces
-    lastworkspace = lastworkspace < 1 ? 1 : lastworkspace 
+    let newworkspace = global.workspace_manager.n_workspaces
 
-        win.change_workspace_by_index(lastworkspace, 1)
-        global.workspace_manager.get_workspace_by_index(lastworkspace).activate(global.get_current_time())
+    win.change_workspace_by_index(newworkspace, 1)
+    global.workspace_manager.get_workspace_by_index(newworkspace).activate(global.get_current_time())
 }
 
 function unmaximize(act) {
-    const win = act.meta_window
-
-    if (win.window_type !== Meta.WindowType.NORMAL)
-        return
-
-    win.change_workspace_by_index(0, 1);
+    act.meta_window.change_workspace_by_index(0, 1);
     global.workspace_manager.get_workspace_by_index(0).activate(global.get_current_time())
 }
 
 function enable() {
     handle = global.window_manager.connect('size-change', (_, act, change) => {
+        if (win.window_type !== Meta.WindowType.NORMAL)
+            return
+
         switch (change) {
             case Meta.SizeChange.FULLSCREEN: return maximize(act)
             case Meta.SizeChange.UNFULLSCREEN: return unmaximize(act)
