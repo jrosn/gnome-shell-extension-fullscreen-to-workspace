30f7d0a45c4402d649cca5fd0bb732efbca9914a
Shiwei Zhang <ylxdzsw@gmail.com>
2019-08-19 19:12:14 +0800

    further simplify

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Forked from https://github.com/satran/fullscreenworkspace-satran.in
 
-- fullscreen will always move the window to a new workspace
-- restored will always move it to workspace0
+- fullscreening will always move the window to a new workspace
+- restoring will always move it to workspace0
 - except that it is on an external display
--- fullscreen-to-workspace@ylxdzsw.com/extension.js
+++ fullscreen-to-workspace@ylxdzsw.com/extension.js
@@ -2,34 +2,30 @@ const Meta = imports.gi.Meta
 
 let handle
 
-function maximize(act) {
-    const win = act.meta_window
-    
-    if (global.workspace_manager.get_active_workspace().list_windows().length == 1)
-        return
-        
-    if (global.display.get_primary_monitor() != win.get_monitor())
-        return
-    
-    let newworkspace = global.workspace_manager.n_workspaces
-
+function maximize(win) {
+    const newworkspace = global.workspace_manager.n_workspaces
     win.change_workspace_by_index(newworkspace, 1)
     global.workspace_manager.get_workspace_by_index(newworkspace).activate(global.get_current_time())
 }
 
-function unmaximize(act) {
-    act.meta_window.change_workspace_by_index(0, 1);
+function unmaximize(win) {
+    win.change_workspace_by_index(0, 1);
     global.workspace_manager.get_workspace_by_index(0).activate(global.get_current_time())
 }
 
 function enable() {
     handle = global.window_manager.connect('size-change', (_, act, change) => {
+        const win = act.meta_window
+
         if (win.window_type !== Meta.WindowType.NORMAL)
             return
 
+        if (global.display.get_primary_monitor() != win.get_monitor())
+            return
+
         switch (change) {
-            case Meta.SizeChange.FULLSCREEN: return maximize(act)
-            case Meta.SizeChange.UNFULLSCREEN: return unmaximize(act)
+            case Meta.SizeChange.FULLSCREEN: return maximize(win)
+            case Meta.SizeChange.UNFULLSCREEN: return unmaximize(win)
         }
     })
 }
